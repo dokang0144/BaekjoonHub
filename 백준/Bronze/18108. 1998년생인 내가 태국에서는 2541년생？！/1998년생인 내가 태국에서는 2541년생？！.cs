e374a65687484c09b3fc4c0ef8b3b8bf3be566f3
dokang0144 <88234731+dokang0144@users.noreply.github.com>
2022-08-01 17:19:40 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace 백준
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        	int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(y-543);
+        }
+    }
+}
